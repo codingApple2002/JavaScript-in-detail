7d7ed18584f0debe3e221d08549f7359fce3ab1a
codingApple2002 <167019083+codingApple2002@users.noreply.github.com>
2024-04-16 08:40:29 +0000

    container config done

---
+++ 01_basics/02_Datatypes&ECMAstandards.js
@@ -0,0 +1,23 @@
+"use strict"; //Engine will treate all Js code as newer version
+
+alert(2+2); //this code exicutable in browser and we are on node js right now...thats y it will throw error
+
+//code readability should be high
+
+
+// Data Types //
+
+//number => range upto 2^53
+//bigint
+//string => " " or ''
+//booleans => true/false
+//null => standalone value or we can say empty value
+//undefined => value not defined...variable got created
+//symbol => unique
+
+//objects
+//arrays
+
+console.log(typeof " "); //output: string
+console.log(typeof null); //output: object
+console.log(typeof undefined); //output: undefined
