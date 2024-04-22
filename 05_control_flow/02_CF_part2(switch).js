94306c4d85ab2316e74a130e8924bcd946dc99a5
codingApple2002 <167019083+codingApple2002@users.noreply.github.com>
2024-04-22 09:27:46 +0000

    CF part2 done

---
+++ 05_control_flow/02_CF_part2(switch).js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march"); //"march"
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case matched");
+        break;
+}
+
+//Note: once a key got matched it will execute the further code, except default. That is why break is imp.
+//here i can match strings as well in a similar way.
+
